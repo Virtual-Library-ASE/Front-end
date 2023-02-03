c12163cd616e9269bb71bd62880596266addf198
chenmo <160594753@qq.com>
2023-02-03 18:15:01 +0000

    prishi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useState } from 'react'
 
 function App() {
   // let isShowLoginDialog = true
-  const [isShowLoginDialog, setShowLoginDialog] = useState(true)
+  const [isShowLoginDialog, setShowLoginDialog] = useState(false)
+  const [isShowSignUpDialog, setShowSignUpDialog] = useState(false)
 
   return (
     <div className="App">
@@ -16,7 +17,13 @@ function App() {
           <div className="nav-item">New Books</div>
           <div className="nav-item">Desks</div>
           <div className="nav-item">Contact Us</div>
-          <div className="nav-item" onClick={() => {setShowLoginDialog(!isShowLoginDialog)}}>Log in</div>
+          <div className="nav-item" onClick={() => {setShowLoginDialog(!isShowLoginDialog)}}>Sign Up</div>
+          <div className="nav-item" onClick={() => {setShowSignUpDialog(!isShowSignUpDialog)}}>Log In </div>
+          <div>
+            <i className="el-icon-edit"></i>
+            <i className="el-icon-share"></i>
+            <i className="el-icon-delete"></i>
+          </div>
         </div>
 
         <div className="banner">
@@ -58,7 +65,27 @@ function App() {
             </div>
             <div className="footer">
               <div className="sign-up">Sign Up</div>
-              <div className="log-in">Login instead?</div>
+              <div className="log-in" onClick={() => { setShowLoginDialog(!isShowLoginDialog) }}>Login instead?</div>
+            </div>
+          </div>
+        </div>
+        <div className={(isShowSignUpDialog ? 'show' : 'hidden') + ' signup-dialog'}>
+          <div className="container">
+            <div className="exit" onClick={() => { setShowSignUpDialog(!isShowSignUpDialog) }}>X</div>
+            <div className="header">
+              Log In
+            </div>
+            <div className="body">
+              <div className="email">
+                <input type="text" className="email-ipt" placeholder="Email address" />
+              </div>
+              <div className="pwd">
+                <input type="text" className="pwd-ipt" placeholder="Create password" />
+              </div>
+            </div>
+            <div className="footer">
+              <div className="sign-up">Log In</div>
+              <div className="log-in" onClick={() => { setShowLoginDialog(!isShowLoginDialog) }}>Sign Up instead?</div>
             </div>
           </div>
         </div>
