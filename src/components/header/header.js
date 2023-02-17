4fbc78d090ce2c0d469c606478df9cffb4a99cf2
chenmo <160594753@qq.com>
2023-02-17 19:10:03 +0000

    lifted header params

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,51 +4,39 @@ import "./header.css";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import PersonIcon from "@mui/icons-material/Person";
 
-class Header extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isActiveIndex: 0,
-      navItems: ["Home", "Books", "Desks", "About", "Contact"],
-    };
-  }
-  render() {
-    return (
-      <div>
-        <header className="header sticky sticky--top js-header">
-          <nav className="navigation">
-            <ul className="nav-list left inline">
-              {this.state.navItems.map((item, index) => (
-                <li
-                  key={index}
-                  className={
-                    "item " +
-                    (this.state.isActiveIndex === index ? "is-active" : "")
-                  }
-                  onClick={() => {
-                    this.setState({ isActiveIndex: index });
-                  }}
-                >
-                  {item}
-                </li>
-              ))}
-            </ul>
+function Header(props) {
+  return (
+    <div>
+      <header className="header sticky sticky--top js-header">
+        <nav className="navigation">
+          <ul className="nav-list left inline">
+            {props.navItems.map((item, index) => (
+              <li
+                key={index}
+                className={
+                  "item " + (props.isActiveIndex === index ? "is-active" : "")
+                }
+                onClick={() => {
+                  props.onClick(index);
+                }}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
 
-            <ul className="nav-list right inline">
-              <li className="item">
-                <a href="#">
-                  <SearchOutlinedIcon sx={{ fontSize: 32 }} />
-                </a>
-              </li>
-              <li className="item">
-                <PersonIcon sx={{ fontSize: 32 }} />
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
-    );
-  }
+          <ul className="nav-list right inline">
+            <li className="item">
+              <SearchOutlinedIcon sx={{ fontSize: 32 }} />
+            </li>
+            <li className="item">
+              <PersonIcon sx={{ fontSize: 32 }} />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,13 +5,31 @@ import Footer from "../../components/footer/footer";
 import logo from "../../resources/images/logo.svg";
 import Button from "@mui/material/Button";
 
-function Home() {
-  return (
-    <div className="Home">
-      <Header />
-      <Footer />
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActiveIndex: 0,
+      navItems: ["Home", "Books", "Desks", "About", "Contact"],
+    };
+  }
+
+  handleNav(index) {
+    this.setState({ isActiveIndex: index });
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <Header
+          isActiveIndex={this.state.isActiveIndex}
+          navItems={this.state.navItems}
+          onClick={(index) => this.handleNav(index)}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Home;
