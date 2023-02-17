1f8348e862acdbe3447fdf9e56a0c679712851ae
chenmo <160594753@qq.com>
2023-02-17 20:08:50 +0000

    add avatar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,13 +3,14 @@ import "./header.css";
 
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import PersonIcon from "@mui/icons-material/Person";
+import Avatar from "@mui/material/Avatar";
 
 function Header(props) {
   return (
     <div>
-      <header className="header sticky sticky--top js-header">
-        <nav className="navigation">
-          <ul className="nav-list left inline">
+      <header className="header text-2xl font-bold sticky sticky--top">
+        <nav className="navigation flex justify-between">
+          <ul className="nav-list flex gap-4">
             {props.navItems.map((item, index) => (
               <li
                 key={index}
@@ -25,12 +26,18 @@ function Header(props) {
             ))}
           </ul>
 
-          <ul className="nav-list right inline">
-            <li className="item">
+          <ul className="nav-list right flex gap-4">
+            <li className="item w-3">
               <SearchOutlinedIcon sx={{ fontSize: 32 }} />
             </li>
-            <li className="item">
+            <li className="item w-3">
               <PersonIcon sx={{ fontSize: 32 }} />
+            </li>
+            <li className="item w-3">
+              <Avatar
+                alt="your avatar"
+                src={require("../../resources/images/avatar.png")}
+              ></Avatar>
             </li>
           </ul>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      'bg-page': '#d0ceba',
-      'bg-card-1': '#012a36',
-      'bg-card-2': '#ff49db',
-      't-title-1': '#ff7849',
-      't-title-2': '#13ce66',
-      't-main-3': '#ffc82c',
-      't-': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      "color-brown": "#934b00",
+      "color-black": "#191201",
+      "color-white": "#ffffff",
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
     extend: {
       spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
+        "8xl": "96rem",
+        "9xl": "128rem",
       },
       borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
-}
+};
