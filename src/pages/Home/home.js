a5e9502b81fc6a47e09a2adf02f710464396d21f
chenmo <160594753@qq.com>
2023-02-23 19:36:53 +0000

    change class component to function component

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,33 +1,29 @@
 import "./home.css";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/header";
 import HomePage from "../HomePage/homepage";
 import Footer from "../../components/footer/footer";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isActiveIndex: 0,
-      navItems: ["Home", "Books", "Desks", "About", "Contact"],
-    };
-  }
+const Home = (props) => {
+  const [state, setState] = useState({
+    isActiveIndex: 0,
+    navItems: ["Home", "Books", "Desks", "About", "Contact"],
+    currPage: props.page,
+  });
 
-  handleNav = (index) => this.setState({ isActiveIndex: index });
+  const handleNav = (index) => setState({ isActiveIndex: index });
 
-  render() {
-    return (
-      <div className="Home">
-        <Header
-          isActiveIndex={this.state.isActiveIndex}
-          navItems={this.state.navItems}
-          onClick={(index) => this.handleNav(index)}
-        />
-        <HomePage />
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Home">
+      <Header
+        isActiveIndex={state.isActiveIndex}
+        navItems={state.navItems}
+        onClick={(index) => handleNav(index)}
+      />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
