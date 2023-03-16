040a715ea4a08f78cf87e40a535928be41724dfd
chenmo <160594753@qq.com>
2023-03-16 17:23:33 +0000

    add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import Router from "./routes/index";
+import store from "./store";
+import { Provider } from "react-redux";
 import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
-      <Router />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Router />
+      </HashRouter>
+    </Provider>
   );
 }
 
