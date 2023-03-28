4685a4830c91b3a0529c0ae41a29da63598002b8
chenmo <160594753@qq.com>
2023-03-28 14:25:37 +0100

    add a sample of get data from database

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,38 @@
 import firebaseConfig from "../firebase";
 
-const ref = firebaseConfig.firestore().collection("book");
-// console.log(ref);
-
-function getBooks() {
+function getBook(id) {
+  const ref = firebaseConfig.firestore().collection("book");
   ref.onSnapshot((querySnapshot) => {
     const items = [];
     querySnapshot.forEach((doc) => {
       items.push(doc.data());
     });
-    // setBooks(items);
-    console.log(items);
+    console.log(items); // 拿到了book里所有数据
+
+    // 遍历所有的数据，找到book_id与传入的id相同的数据，并返回它
+    const res = [];
+    for (let i = 0; i < items.length; i++) {
+      if (items[i]["book_id"] === id) {
+        res.push(items[i]);
+      }
+    }
+
+    // print results
+    console.log({
+      status: 200,
+      msg: "ok",
+      data: res,
+    });
+
+    return {
+      status: 200,
+      msg: "ok",
+      data: res,
+    };
   });
 }
+
+getBook("10001");
 
 /**
  *
@@ -24,4 +44,4 @@ function getBookRecommendList(name, count) {
   return;
 }
 
-export { getBooks, getBookRecommendList };
+export { getBook, getBookRecommendList };
