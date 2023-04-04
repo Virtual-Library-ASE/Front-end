cd8fef61483c10e3f4f734c947e1d5f99c8efd6b
chenmo <160594753@qq.com>
2023-04-04 12:52:49 +0100

    fixed some style problem

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,16 @@ import firebase from "firebase/compat/app";
 // import "firebase/auth";
 import "firebase/compat/firestore";
 
-
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyCbPqJ-hlCHlV0XRGxNlweeDGVu973C3qk",
-    authDomain: "fir-test-project-bae00.firebaseapp.com",
-    databaseURL: "https://fir-test-project-bae00-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fir-test-project-bae00",
-    storageBucket: "fir-test-project-bae00.appspot.com",
-    messagingSenderId: "592761039577",
-    appId: "1:592761039577:web:63fcce6a394813ef79ecc8",
-    measurementId: "G-4VVZ3MG1JP"
-  });
+  apiKey: "AIzaSyCbPqJ-hlCHlV0XRGxNlweeDGVu973C3qk",
+  authDomain: "fir-test-project-bae00.firebaseapp.com",
+  databaseURL:
+    "https://fir-test-project-bae00-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-test-project-bae00",
+  storageBucket: "fir-test-project-bae00.appspot.com",
+  messagingSenderId: "592761039577",
+  appId: "1:592761039577:web:63fcce6a394813ef79ecc8",
+  measurementId: "G-4VVZ3MG1JP",
+});
 
-  export default firebaseConfig;
+export default firebaseConfig;
